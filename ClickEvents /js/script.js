6b0313810b4f657eed2142160fcaf7fcbc1ee019
itylermcmanus <tyler.mcmanus1@icloud.com>
2018-08-23 19:11:52 -0500

    Add files via upload

---
+++ ClickEvents /js/script.js
@@ -0,0 +1,641 @@
+$(document).ready(function() {
+  var hue1 = 'rgb(' + (Math.floor(Math.random() * 256 )) + ',' + (Math.floor(Math.random() * 256 )) + ',' +(Math.floor(Math.random() * 256 )) + ')'; 
+  var hue2 = 'rgb(' + (Math.floor(Math.random() * 256 )) + ',' + (Math.floor(Math.random() * 256 )) + ',' +(Math.floor(Math.random() * 256 )) + ')'; 
+  var judge = true;
+  var kb=0;
+
+
+  $(".kb").hover(function(){
+    if (judge == true) {
+      $(this).animate({backgroundColor: hue1}, "600");
+      // kb++;
+      judge = false;
+    }else {
+      $(this).animate({backgroundColor: hue2}, "600");
+      // kb=1;
+      judge = true;
+    }
+
+
+  });
+
+
+
+
+  // var ad = Math.round(Math.random() * 255);
+  // var bd = Math.round(Math.random() * 255);
+  //var cd = Math.round(Math.random() * 255);
+  $( "body" ).animate({backgroundColor: hue1}, "600");
+
+
+  var color = ["	#7fffd4", "orange", "yellow", "blue", "#00F3FF ", "#8BFF00 ", "#AA00E8 ", "white"];
+  var random = Math.round(Math.random() * 7);
+  $( ".kb" ).css("background-color", hue2);
+
+  var color1 = ["#FF5F00 ", "#6495ed", "	#bdb76b", "	#ff1493", "	#fffaf0", "#e6e6fa ", "	#ffa07a ", "#ba55d3"];
+  var random1 = Math.round(Math.random() * 7);
+
+
+  $('#box1').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+  $('#box2').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box3').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+  $('#box4').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+  $('#box5').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+
+  $('#box6').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  }); 
+
+
+  $('#box7').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+
+  $('#box8').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+  $('#box9').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+
+  $('#box10').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box11').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+  $('#box12').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box13').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+  $('#box14').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+
+  $('#box15').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box16').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box17').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  }); 
+
+
+  $('#box18').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box19').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box20').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+
+
+
+  $('#box21').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box22').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+  $('#box23').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box24').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+  $('#box25').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+
+  $('#box26').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  }); 
+
+
+  $('#box27').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+
+  $('#box28').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+  $('#box29').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+
+  $('#box30').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box31').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+  $('#box32').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box33').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+  $('#box34').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+
+  $('#box35').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box36').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box37').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  }); 
+
+
+  $('#box38').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box39').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box40').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+  $('#box41').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box42').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+  $('#box43').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box44').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+  $('#box45').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+
+  $('#box46').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  }); 
+
+
+  $('#box47').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+
+  $('#box48').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+  $('#box49').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+
+  $('#box50').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box51').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+  $('#box52').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box53').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+  $('#box54').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+
+  $('#box55').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box56').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box57').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  }); 
+
+
+  $('#box58').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box59').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box60').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+  $('#box61').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+  $('#box62').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box63').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+  $('#box64').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+
+  $('#box65').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box66').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box67').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  }); 
+
+
+  $('#box68').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+  });
+
+
+  $('#box69').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box70').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+  $('#box71').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box72').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+  $('#box73').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+  });
+
+
+  $('#box74').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+  $('#box75').hover(function() {
+    $(this).addClass("animated" );
+    $(this).addClass("shake" );
+    $(this).addClass("infinite" );
+
+
+  });
+
+
+
+});
